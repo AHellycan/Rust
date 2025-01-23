9264d6ebb8f24e6cb6255930375858d62cbbdea0
AHellycan <gie.moreno.c@gmail.com>
2025-01-24 00:54:39 +0100

    First point, mean calculation done

--- Basic_Exercises/Rust_Book/Unit_8/meancalc/src/main.rs
+++ Basic_Exercises/Rust_Book/Unit_8/meancalc/src/main.rs
@@ -1,3 +1,21 @@
+fn mean(v: Vec<i32>)-> f64 {
+    let mut sum=0;
+    let mut index=0;
+
+    for i in v{
+        sum= sum+i;
+        //print!("{}",i);
+        index +=1;
+    }
+    sum as f64/ index as f64
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut v_list = Vec::new();
+    v_list.push(5);
+    v_list.push(3);
+    v_list.push(4);
+    v_list.push(9);    
+    
+    println!("The mean is {}",mean(v_list));
 }
