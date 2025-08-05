d889638abae32abe798e57432db6b65c44e41b6e
AHellycan <gie.moreno.c@gmail.com>
2025-08-05 23:39:21 +0200

    New project CLI Jira from bootcamp resolved

---
+++ Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/intermediate_clijira-197a1a13d95402f3.d
@@ -0,0 +1,5 @@
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/libintermediate_clijira-197a1a13d95402f3.rmeta: src/main.rs
+
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/intermediate_clijira-197a1a13d95402f3.d: src/main.rs
+
+src/main.rs:
---
+++ Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/intermediate_clijira-dda53b9f343c95a7.d
@@ -0,0 +1,5 @@
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/libintermediate_clijira-dda53b9f343c95a7.rmeta: src/main.rs
+
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/intermediate_clijira/target/debug/deps/intermediate_clijira-dda53b9f343c95a7.d: src/main.rs
+
+src/main.rs:
