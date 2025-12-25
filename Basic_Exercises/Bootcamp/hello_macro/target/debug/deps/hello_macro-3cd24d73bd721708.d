f662dd2b1bddf14756cda190fa7beddf765fd401
AHellycan <gie.moreno.c@gmail.com>
2025-12-25 22:36:24 +0100

    first steps for macros

---
+++ Basic_Exercises/Bootcamp/hello_macro/target/debug/deps/hello_macro-3cd24d73bd721708.d
@@ -0,0 +1,5 @@
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/hello_macro/target/debug/deps/hello_macro-3cd24d73bd721708.d: src/lib.rs
+
+/home/archnime/Documentos/GIT/Rust/Basic_Exercises/Bootcamp/hello_macro/target/debug/deps/libhello_macro-3cd24d73bd721708.rmeta: src/lib.rs
+
+src/lib.rs:
