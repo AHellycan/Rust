58ac1352b95ef765ec376aa14c277e711edc0e26
AHellycan <gie.moreno.c@gmail.com>
2026-03-18 00:30:46 +0100

    Chapter 1 book done

--- Cybersecurity/Black_Hat_Rust/Chapter_01/sha1_cracker/src/main.rs
+++ Cybersecurity/Black_Hat_Rust/Chapter_01/sha1_cracker/src/main.rs
@@ -1,4 +1,5 @@
-use std::{env,error::Error};
+use sha1::Digest;
+use std::{env,error::Error,fs::File,io::{BufRead,BufReader}};
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
 
@@ -17,5 +18,17 @@ fn main() -> Result<(),Box<dyn Error>>{
         return Err("sha1 hash is not valid!".into());
     }
 
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines(){
+        let line = line?;
+        let common_password=line.trim();
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())){
+            println!("Password found: {}",&common_password);
+            return Ok(());
+        }    
+    }
+    println!("Password not found in list");
     Ok(())
 }
